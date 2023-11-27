9b3bd7e9b097025e518b38354d17a623f6c1dc18
vadimlol22 <vadimlol22@bk.ru>
2023-11-27 13:48:44 +0300

    update workout logs

--- app/workout/workout.routes.js
+++ app/workout/workout.routes.js
@@ -2,8 +2,8 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
-// import { getWorkoutLog } from './log/get-workout-log.controller.js'
-// import { updateCompleteWorkoutLog } from './log/update-workout-log.controller.js'
+import { getWorkoutLog } from './log/get-workout-log.controller.js'
+import { updateCompleteWorkoutLog } from './log/update-workout-log.controller.js'
 import { createNewWorkoutLog } from './log/workout-log.controller.js'
 import {
 	createNewWorkout,
@@ -23,9 +23,11 @@ router
 	.put(protect, updateWorkout)
 	.delete(protect, deleteWorkout)
 
-router.route('/log/:id').post(protect, createNewWorkoutLog)
-// .get(protect, getWorkoutLog)
+router
+	.route('/log/:id')
+	.post(protect, createNewWorkoutLog)
+	.get(protect, getWorkoutLog)
 
-// router.route('/log/complete/:id').patch(protect, updateCompleteWorkoutLog)
+router.route('/log/complete/:id').patch(protect, updateCompleteWorkoutLog)
 
 export default router
